29ee7aed1dc93c04fcc543f215433be39cbadec7
EwenIllirian9138 <ewen.billot@orange.fr>
2018-08-27 17:26:26 +0200

    retrait des using inutile

--- Librairie/RequestAPI.cs
+++ Librairie/RequestAPI.cs
@@ -1,14 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
-using System.Globalization;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
-using System.Collections;
 
 namespace Librairie
 {
